0480e14d6babf35dc34a7dae0692beb7e95d3149
Kmek <kmekeefe@gmail.com>
2023-02-21 12:01:00 -0500

    Add print tag, lang, and copylist methods

--- helper.js
+++ helper.js
@@ -29,7 +29,7 @@ function replace(folder, fileToCloneName, stringToFind, copyList, doOverwriteFil
     } catch (err) {
         console.error(err);
     }
-    if (fileToCloneContents == null || fileToCloneName.indexOf(stringToFind) == -1 || fileToCloneContents.indexOf(stringToFind) == -1) {
+    if (fileToCloneContents == null || fileToCloneName.indexOf(stringToFind) == -1) {
         console.log("Error: file '" + fileToCloneName +"' has no string '" + stringToFind + "' to replace!");
         return;
     }
@@ -64,4 +64,112 @@ function replace(folder, fileToCloneName, stringToFind, copyList, doOverwriteFil
     console.log("\nfinished :)");
 }
 
-module.exports = { replace };
+// Prints formatted english lang file entries that can be pasted into the lang file
+function printLang(folder, fileToCloneName, stringToFind, copyList, modid) {
+    // Check folder exists
+    if (!fs.existsSync(folder)) {
+        console.log("Error: no dir '" + folder +"' found!");
+        return;
+    }
+
+    // Check that copyList has elements
+    copyList = Array.from(copyList);
+    if (copyList.length == 0) {
+        console.log("Error: copyList has no elements!");
+        return;
+    }
+
+    // Check file for copying exists
+    let fileToClonePath = path.resolve(folder, fileToCloneName);
+    if (!fs.existsSync(fileToClonePath)) {
+        console.log("Error: no file '" + fileToClonePath +"' found!");
+        return;
+    }
+
+    // Get contents of file to copy
+    let fileToCloneContents = null;
+    try {
+        fileToCloneContents = fs.readFileSync(fileToClonePath, 'utf8');
+    } catch (err) {
+        console.error(err);
+    }
+    if (fileToCloneContents == null || fileToCloneName.indexOf(stringToFind) == -1) {
+        console.log("Error: file '" + fileToCloneName +"' has no string '" + stringToFind + "' to replace!");
+        return;
+    }
+
+    console.log("\nFormatted English Lang:");
+
+    copyList.forEach(stringToReplace => {
+        let newFileName = fileToCloneName.replace(stringToFind, stringToReplace).replace(".json", "");
+        let nameOfItem = newFileName.split("_").map(f => {
+            return f[0].toUpperCase() + f.slice(1);
+        }).join(" ");
+
+        console.log("  \"item." + modid + "." + newFileName + "\": \"" + nameOfItem + "\",");
+    });
+    console.log("\nfinished :)");
+}
+
+// Prints a list of the items that can be pasted into a tag json file
+function printTag(folder, fileToCloneName, stringToFind, copyList, modid) {
+    // Check folder exists
+    if (!fs.existsSync(folder)) {
+        console.log("Error: no dir '" + folder +"' found!");
+        return;
+    }
+
+    // Check that copyList has elements
+    copyList = Array.from(copyList);
+    if (copyList.length == 0) {
+        console.log("Error: copyList has no elements!");
+        return;
+    }
+
+    // Check file for copying exists
+    let fileToClonePath = path.resolve(folder, fileToCloneName);
+    if (!fs.existsSync(fileToClonePath)) {
+        console.log("Error: no file '" + fileToClonePath +"' found!");
+        return;
+    }
+
+    console.log("\nFormatted Tags:");
+
+    // Get contents of file to copy
+    let fileToCloneContents = null;
+    try {
+        fileToCloneContents = fs.readFileSync(fileToClonePath, 'utf8');
+    } catch (err) {
+        console.error(err);
+    }
+    if (fileToCloneContents == null || fileToCloneName.indexOf(stringToFind) == -1) {
+        console.log("Error: file '" + fileToCloneName +"' has no string '" + stringToFind + "' to replace!");
+        return;
+    }
+
+    copyList.forEach(stringToReplace => {
+        let newFileName = fileToCloneName.replace(stringToFind, stringToReplace).replace(".json", "");
+        console.log("  \"" + modid + ":" + newFileName + "\",");
+    });
+    console.log("\nfinished :)");
+}
+
+function printCopyList(copyList) {
+    // Check that copyList has elements
+    copyList = Array.from(copyList);
+    if (copyList.length == 0) {
+        console.log("Error: copyList has no elements!");
+        return;
+    }
+
+    console.log("\nCopyList Array of Strings:")
+
+    let stringList = ""
+    copyList.forEach(str => {
+        stringList += "\"" + str + "\", "
+    })
+    console.log(stringList)
+}
+
+
+module.exports = { replace, printLang, printTag, printCopyList };
--- runner.js
+++ runner.js
@@ -1,21 +1,27 @@
 
-const { replace } = require("./helper.js");
+const { replace, printLang, printTag, printCopyList } = require("./helper.js");
 const { copyList } = require("./copylist.js");
 var path = require('path');
 const fs = require('fs');
+
+/******************** Folder Setup ********************/
 
 // Folder Paths
 var resourcesFolder = path.resolve("C:\\Absolute\\Path\\To\\Your\\src\\main\\resources"); // todo Required: Must fill in
 var blockstatesFolder = path.resolve(resourcesFolder, './assets/minecafe/blockstates/');
 var blockModelsFolder = path.resolve(resourcesFolder, './assets/minecafe/models/block/');
 var itemModelsFolder = path.resolve(resourcesFolder, './assets/minecafe/models/item/');
-var recipesFolder = path.resolve(resourcesFolder, './data/minecafe/recipe/');
+var loottablesFolder = path.resolve(resourcesFolder, './data/minecafe/loot_tables/blocks/');
+var recipesFolder = path.resolve(resourcesFolder, './data/minecafe/recipes/');
+var recipesFolderSpecific = path.resolve(recipesFolder, './ingredients/pies');
 
 // Print what strings are in the copyList (Optional)
 // console.log(copyList);
 
 // Print what a folder has in it (Optional)
-// fs.readdirSync(blockstatesFolder).forEach(str => console.log("\t- " + str))
+// fs.readdirSync(loottablesFolder).forEach(str => console.log("\t- " + str))
+
+/******************** Script Calls ********************/
 
 // Clone files for a list of string variants
 // todo must make one call to replace or the script won't create any JSON files
